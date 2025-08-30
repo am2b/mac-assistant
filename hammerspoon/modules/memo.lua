59f41e2bafea5af24d8ed54ef7330d9544bd8065
am2b <20837051+am2b@users.noreply.github.com>
2025-08-30 18:43:27 +0800

    update modules/memo.lua

--- hammerspoon/modules/memo.lua
+++ hammerspoon/modules/memo.lua
@@ -2,34 +2,51 @@
 
 local module = {}
 
-local escBinding = nil
+local escTap = nil
 local memoWatcher = nil
 
-function module.start()
-    local function bindEsc()
-        if escBinding == nil then
-            --hs.alert.show("备忘录:绑定ESC -> cmd + shift + e")
-            escBinding = hs.hotkey.bind({}, "escape", function()
-                -- 高亮标记被选中的文本
+local function bindEscEvent()
+    if escTap == nil then
+        escTap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
+            if hs.window.focusedWindow():application():name() ~= "备忘录" then return false end
+            if event:getKeyCode() == hs.keycodes.map["escape"] then
+                --高亮标记被选中的文本
                 hs.eventtap.keyStroke({ "cmd", "shift" }, "e")
-            end)
-        end
+                return true
+            end
+            return false
+        end)
+        escTap:start()
+        hs.printf("ESC 热键已绑定")
     end
+end
 
-    local function unbindEsc()
-        if escBinding ~= nil then
-            --hs.alert.show("备忘录:解绑ESC")
-            escBinding:delete()
-            escBinding = nil
-        end
+local function unbindEscEvent()
+    if escTap then
+        escTap:stop()
+        escTap = nil
+        hs.printf("ESC 热键已解绑")
     end
+end
 
+function module.start()
     --使用hs.window.filter API监控"备忘录"的窗口焦点变化事件,并在窗口聚焦和失焦时分别执行相应的函数
     --创建一个新的窗口过滤器实例memoWatcher,用来监控名字为"备忘录"的应用窗口
     memoWatcher = hs.window.filter.new("备忘录")
     --subscribe方法用于订阅特定的窗口事件,当监控的窗口发生这些事件时会触发指定的回调函数
-    memoWatcher:subscribe(hs.window.filter.windowFocused, bindEsc)
-    memoWatcher:subscribe(hs.window.filter.windowUnfocused, unbindEsc)
+    memoWatcher:subscribe(hs.window.filter.windowFocused, function()
+        --延迟一点确保窗口稳定
+        hs.timer.doAfter(0.2, bindEscEvent)
+    end)
+    memoWatcher:subscribe(hs.window.filter.windowUnfocused, unbindEscEvent)
+end
+
+function module.stop()
+    if memoWatcher then
+        memoWatcher:unsubscribeAll()
+        memoWatcher = nil
+    end
+    unbindEscEvent()
 end
 
 return module
